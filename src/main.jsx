b99159f779b773932b95ba2c6a4b0a32e4762580
mnovak-20 <matthew.novak@staffs.ac.uk>
2025-07-22 08:25:33 +0100

    gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename="/staffs.games">
+        <BrowserRouter basename="/">
 
         <App />
         </BrowserRouter>
