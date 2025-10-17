0c804dd1a1473b679dd4855600e4e303089e9f97
mnovak-20 <matthew.novak@staffs.ac.uk>
2025-10-17 15:59:55 +0100

    updated studios / fixed responsivness

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import './style.css'
 import App from './App.jsx'
 
@@ -9,8 +9,8 @@ window.Buffer = Buffer;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename="/staffs.games/">
-        <App />
-        </BrowserRouter>
+        <HashRouter>
+            <App />
+        </HashRouter>
     </React.StrictMode>
 );
